7fb9b461a3e330a1cb20f08e77936711a2c14f5a
zlutfi <33373366+zlutfi@users.noreply.github.com>
2020-09-25 10:25:43 -0400

    renaming function

--- src/scrapers/trails/trailParse.js
+++ src/scrapers/trails/trailParse.js
@@ -1,18 +1,18 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 
-const potusParse = function(url) {
-  return rp(url)
-    .then(function(html) {
-      return {
-        name: $('.firstHeading', html).text(),
-        status: $('.bday', html).text(),
-        date: $('.bday', html).text()
-      };
-    })
-    .catch(function(err) {
-      //handle error
-    });
+const trailParse = function(url) {
+	return rp(url)
+		.then(function(html) {
+			return {
+				name: $('.firstHeading', html).text(),
+				status: $('.bday', html).text(),
+				date: $('.bday', html).text()
+			};
+		})
+		.catch(function(err) {
+			//handle error
+		});
 };
 
-module.exports = potusParse;
+module.exports = trailParse;

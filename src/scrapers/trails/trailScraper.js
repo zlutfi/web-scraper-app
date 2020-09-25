2c16036e4093b921dd3fff15d042e559b7eba1f4
zlutfi <33373366+zlutfi@users.noreply.github.com>
2020-09-25 10:25:57 -0400

    adjusting params

--- src/scrapers/trails/trailScraper.js
+++ src/scrapers/trails/trailScraper.js
@@ -3,18 +3,18 @@ const $ = require('cheerio');
 const url = 'http://tarheeltrailblazers.com/mobile/';
 
 rp(url)
-  .then(function(html) {
-    //success!
-    const trailStatus = [];
-    for (let i = 0; i < 17; i++) {
-      trailStatus.push(
-        $('td > b', html)[i].children,
-        $('font > strong', html)[i].children,
-        $('td > em', html)[i].children
-      );
-    }
-    console.log(trailStatus);
-  })
-  .catch(function(err) {
-    //handle error
-  });
+	.then(function(html) {
+		//success!
+		const trailStatus = [];
+		for (let i = 0; i < 18; i++) {
+			trailStatus.push(
+				$('td > b', html)[i].children,
+				$('td> font > strong', html)[i].children,
+				$('td > em', html)[i].children
+			);
+		}
+		console.log(trailStatus);
+	})
+	.catch(function(err) {
+		//handle error
+	});

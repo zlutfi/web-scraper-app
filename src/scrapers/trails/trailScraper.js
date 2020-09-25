d4326363064edb071843b1a58e287a2cfb002234
zlutfi <33373366+zlutfi@users.noreply.github.com>
2020-09-25 19:00:26 -0400

    updated to work with new packages

--- src/scrapers/trails/trailScraper.js
+++ src/scrapers/trails/trailScraper.js
@@ -1,20 +1,41 @@
-const rp = require('request-promise');
-const $ = require('cheerio');
-const url = 'http://tarheeltrailblazers.com/mobile/';
+	const axios = require('axios');
+	const cheerio = require('cheerio');
 
-rp(url)
-	.then(function(html) {
-		//success!
-		const trailStatus = [];
-		for (let i = 0; i < 18; i++) {
-			trailStatus.push(
-				$('td > b', html)[i].children,
-				$('td> font > strong', html)[i].children,
-				$('td > em', html)[i].children
-			);
-		}
-		console.log(trailStatus);
-	})
-	.catch(function(err) {
-		//handle error
-	});
+    const url = 'http://tarheeltrailblazers.com'; // The URL to scrape
+
+    axios(url)
+      .then(response => {
+		const html = response.data;
+		const $ = cheerio.load(html);
+		const trailStatusWrapper = $('.row > div');
+		const trailInfo = []; // Holds the parsed trail information
+		
+		trailStatusWrapper.each(function () {
+			// Searches for the class and saves the text value to a variable
+			let trailName = $(this).find('.tinyTrailStatusTitle').text();
+			let trailStatus = $(this).find('.sr-only').text();
+			let trailUpdated = $(this).find('.tinyTrailStatusUpdated').text();
+
+			// Update the array with the new entries
+			trailInfo.push({
+				name: trailName,
+				status: trailStatus,
+				updated: trailUpdated,
+			})
+		})
+
+		// const trailName = $('.tinyTrailStatusTitle').text();
+		// const trailStatus = $('.sr-only').text();
+		// const trailStatusUpdated = $('.tinyTrailStatusUpdated').text();
+		// const trailStatusWrapper = {
+		// 	"name": trailName,
+		// 	"status": trailStatus,
+		// 	"updated": trailStatusUpdated
+		// }
+
+
+		// Log the result to the console for testing
+        console.log(trailInfo);
+      })
+	  .catch(console.error);
+	  

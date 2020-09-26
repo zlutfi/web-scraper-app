276e3be2aaea498dfb5efe1c67f03f4fc74fb688
zlutfi <33373366+zlutfi@users.noreply.github.com>
2020-09-26 11:56:20 -0400

    removing empty entries

--- src/scrapers/trails/trailScraper.js
+++ src/scrapers/trails/trailScraper.js
@@ -1,41 +1,57 @@
-	const axios = require('axios');
-	const cheerio = require('cheerio');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-    const url = 'http://tarheeltrailblazers.com'; // The URL to scrape
+const url = 'http://tarheeltrailblazers.com'; // The URL to scrape
 
-    axios(url)
-      .then(response => {
+axios(url)
+	.then(response => {
 		const html = response.data;
 		const $ = cheerio.load(html);
 		const trailStatusWrapper = $('.row > div');
 		const trailInfo = []; // Holds the parsed trail information
-		
+
 		trailStatusWrapper.each(function () {
-			// Searches for the class and saves the text value to a variable
+
+			function isEmpty(obj) {
+				for (var key in obj) {
+					if (obj.hasOwnProperty(key))
+						return false;
+				}
+				return true;
+			}
+			// function findSelector (arr) {
+			// 	var selector = arr;
+			// 	var find = $(this).find(selector).text();
+			// 	return find
+			// }
+
+			// // Searches for the class and saves the text value to a variable
+			// let trailName = findSelector('.tinyTrailStatus');
+			// let trailStatus = findSelector('.sr-only');
+			// let trailUpdated = findSelector('.tinyTrailStatusUpdated');
 			let trailName = $(this).find('.tinyTrailStatusTitle').text();
 			let trailStatus = $(this).find('.sr-only').text();
 			let trailUpdated = $(this).find('.tinyTrailStatusUpdated').text();
 
-			// Update the array with the new entries
-			trailInfo.push({
-				name: trailName,
-				status: trailStatus,
-				updated: trailUpdated,
-			})
-		})
 
-		// const trailName = $('.tinyTrailStatusTitle').text();
-		// const trailStatus = $('.sr-only').text();
-		// const trailStatusUpdated = $('.tinyTrailStatusUpdated').text();
-		// const trailStatusWrapper = {
-		// 	"name": trailName,
-		// 	"status": trailStatus,
-		// 	"updated": trailStatusUpdated
-		// }
+			// Delete empty entries
+			if (isEmpty(trailName, trailStatus, trailUpdated)) {
+				// Object is empty 
+				delete trailName, trailStatus, trailUpdated
+			} else {
+				// Object is NOT empty
+				trailInfo.push({
+					name: trailName,
+					status: trailStatus,
+					updated: trailUpdated
+				})
+
+			}
+
+		})
 
 
 		// Log the result to the console for testing
-        console.log(trailInfo);
-      })
-	  .catch(console.error);
-	  
+		console.log(trailInfo);
+	})
+	.catch(console.error);
